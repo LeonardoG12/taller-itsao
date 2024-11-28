f753be042f7f612ba3a71b9e7167621eee50f0d8
LeonardoG12 <leoggarciamejia@gmail.com>
2024-11-27 18:17:38 -0600

    agregando proyecto completo

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Proyecto </title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <div class="container-fluid" style="background-color: #752454; text-align: center; padding:5px
+  ;">
+    <h1 style="color: white;">Bienvenidos a ITSAO</h1>
+    <video autoplay muted loop controls width="700">
+        <source src="Cliente/img/vid1.mp4" type="video/mp4">
+        Tu navegador no soporta la etiqueta de video.
+    </video>
+  </div>
+
+  <div class="container" style="margin-top: 15px; background-color: #d957a3; padding: 10px;">
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
